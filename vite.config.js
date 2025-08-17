555e245d21afb4aa6b93bce2d325ad774437cd37
ishikajais27 <ishikajais09876@gmail.com>
2025-08-17 21:17:58 +0530

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx', // Explicit entry point
+      },
+    },
+  },
 })
